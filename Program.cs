bc8ec382b873f4c726eccf9e368909083a886d7f
Sinoroth <denispredut@yahoo.com>
2018-03-23 00:42:32 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1ProducerConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly program = new Assembly();
+            program.run();
+
+        }
+    }
+}
